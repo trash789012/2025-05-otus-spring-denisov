c43847186f2d18bb43f8da3243c10bd44135b87f
trash789012@gmail.com <trash789012@gmail.com>
2025-06-10 09:48:07 +0500

    реализовано dao, подключено использование в executeService

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,78 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+    private static final String FILE_NOT_FOUND = "File Not Found";
+
+    private static final String INCORRECT_EXTENSION = "Extension not csv";
+
+    private static final String FILE_NOT_LOADED = "File Not Loaded";
+
+    private static final String ERROR_READING = "Error Reading File";
+
+    private static final String ERROR_PARSING = "Error Pasring File";
+
+    private static final String EMPTY_QUESTION_LIST = "Empty Question List";
+
+    private static final int SKIP_ROW_COUNT = 1;
+
+    private static final char SEPARATOR = ';';
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        try (var inputStream = getInputStream();
+             var reader = new BufferedReader(new InputStreamReader(inputStream))) {
 
-        return new ArrayList<>();
+            CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withSeparator(SEPARATOR)
+                    .withSkipLines(SKIP_ROW_COUNT)
+                    .withType(QuestionDto.class)
+                    .build();
+
+            List<QuestionDto> questionDtoList = csvToBean.stream().toList();
+            if (questionDtoList.isEmpty()) {
+                throw new QuestionReadException(EMPTY_QUESTION_LIST);
+            }
+
+            return questionDtoList.stream().map(QuestionDto::toDomainObject).toList();
+        } catch (IOException e) {
+            throw new QuestionReadException(ERROR_READING, e);
+        } catch (RuntimeException e) {
+            throw new QuestionReadException(ERROR_PARSING, e);
+        }
+    }
+
+    private InputStream getInputStream() throws IOException {
+        var path = fileNameProvider.getTestFileName();
+        if (path.isEmpty() || !path.endsWith(".csv") || path == null) {
+            throw new IOException(FILE_NOT_FOUND);
+        }
+
+        if (!path.endsWith(".csv")) {
+            throw new IOException(INCORRECT_EXTENSION);
+        }
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(path);
+        if (inputStream == null) {
+            throw new IOException(FILE_NOT_LOADED + path);
+        }
+
+        return inputStream;
     }
 }
--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,20 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.dao.QuestionDao;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
-
     private final IOService ioService;
+
+    private final QuestionDao questionDao;
 
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
-        // Получить вопросы из дао и вывести их с вариантами ответов
+
+        var questions = questionDao.findAll();
+
     }
 }
