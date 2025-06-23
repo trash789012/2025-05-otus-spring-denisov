18404e02c0753d68376bcc0c1cb1be27ada40d66
trash789012@gmail.com <trash789012@gmail.com>
2025-06-23 16:02:26 +0500

    аннотирование

--- hw02/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw02/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -2,8 +2,10 @@ package ru.otus.hw.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class AppProperties implements TestConfig, TestFileNameProvider {
 
     // внедрить свойство из application.properties
