7f29e9ddecfeb948acff3df4eb3310328d901519
trash789012@gmail.com <trash789012@gmail.com>
2025-06-26 17:22:19 +0500

    Оптимизация import

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
