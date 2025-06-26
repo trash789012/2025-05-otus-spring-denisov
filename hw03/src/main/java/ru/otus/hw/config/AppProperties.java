df54e22667d391bfb75d35ca5f1532e99164bb0d
trash789012@gmail.com <trash789012@gmail.com>
2025-06-26 14:19:59 +0500

    запуск без фатальных ошибок

--- hw03/src/main/java/ru/otus/hw/config/AppProperties.java
+++ hw03/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 import java.util.Map;
 
 @Setter
-@ConfigurationProperties
+@ConfigurationProperties(prefix = "test")
 public class AppProperties implements TestConfig, TestFileNameProvider, LocaleConfig {
 
     @Getter
