220aeb892b1180be6820a4722ecaff6e6174f122
trash789012@gmail.com <trash789012@gmail.com>
2025-06-30 12:39:55 +0500

    оптимизация import в тестах

--- hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
+++ hw03/src/test/java/ru/otus/hw/service/TestServiceImplTest.java
@@ -15,7 +15,9 @@ import ru.otus.hw.service.converter.QuestionConverter;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
