ff524c90c46255bbe320f247132fc9b6102ebd63
trash789012@gmail.com <trash789012@gmail.com>
2025-07-09 20:27:03 +0500

    h2 console и jdbc для авторов и жанров

--- hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -1,33 +1,58 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Author;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcOperations jdbcOperations;
 
     @Override
     public List<Author> findAll() {
-        return new ArrayList<>();
+        //language=sql
+        String sql = """
+                SELECT 
+                    id, 
+                    full_name 
+                  FROM authors;
+                """;
+        return jdbcOperations.query(sql, new AuthorRowMapper());
     }
 
     @Override
     public Optional<Author> findById(long id) {
-        return Optional.empty();
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("id", id);
+
+        //language=sql
+        String sql = """
+                SELECT 
+                    id,
+                    full_name
+                  FROM authors
+                  WHERE id = :id
+                """;
+        return jdbcOperations.query(sql, params, new AuthorRowMapper()).stream().findFirst();
     }
 
     private static class AuthorRowMapper implements RowMapper<Author> {
 
         @Override
         public Author mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Author(
+                    rs.getLong("id"),
+                    rs.getString("full_name")
+            );
         }
     }
 }
--- hw05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
+++ hw05/src/main/java/ru/otus/hw/repositories/JdbcGenreRepository.java
@@ -1,33 +1,63 @@
 package ru.otus.hw.repositories;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
 import ru.otus.hw.models.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
 @Repository
+@RequiredArgsConstructor
 public class JdbcGenreRepository implements GenreRepository {
+
+    private final NamedParameterJdbcOperations jdbcOperations;
 
     @Override
     public List<Genre> findAll() {
-        return new ArrayList<>();
+        //language=sql
+        String sql = """
+                  SELECT 
+                      id,
+                      name
+                    FROM genres
+                """;
+        return jdbcOperations.query(sql, new GnreRowMapper());
     }
 
     @Override
     public List<Genre> findAllByIds(Set<Long> ids) {
-        return new ArrayList<>();
+        if (ids.isEmpty()){
+            return List.of();
+        }
+
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("ids", ids);
+
+        //language=sql
+        String sql = """
+                  SELECT 
+                      id, 
+                      name
+                    FROM genres
+                    WHERE id IN (:ids)
+                """;
+        return jdbcOperations.query(sql, params, new GnreRowMapper());
     }
 
     private static class GnreRowMapper implements RowMapper<Genre> {
 
         @Override
         public Genre mapRow(ResultSet rs, int i) throws SQLException {
-            return null;
+            return new Genre(
+                    rs.getLong("id"),
+                    rs.getString("name")
+            );
         }
     }
 }
