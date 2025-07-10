580b7b08719a226d4fe2cac1ea4018ee3c45aefc
trash789012@gmail.com <trash789012@gmail.com>
2025-07-10 16:02:40 +0500

    Тесты для дао жанров

---
+++ hw05/src/test/java/ru/otus/hw/repositories/JdbcDenreRepositoryTest.java
@@ -0,0 +1,46 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Репозиторий на основе Jdbc для работы с жанрами")
+@JdbcTest
+@Import({JdbcGenreRepository.class})
+public class JdbcDenreRepositoryTest {
+
+    private List<Genre> dbGenres;
+
+    @BeforeEach
+    void setUp() {
+        dbGenres = getDbGenres();
+    }
+
+    @Autowired
+    private JdbcGenreRepository jdbcGenreRepository;
+
+    @DisplayName("Должен загружать список всех жанров")
+    @Test
+    void shouldReturnCorrectGenres() {
+        var actualGenres = jdbcGenreRepository.findAll();
+        var expectedGenres = dbGenres;
+
+        assertThat(actualGenres).containsExactlyElementsOf(expectedGenres);
+    }
+
+    private List<Genre> getDbGenres() {
+        return IntStream.range(1, 7).boxed()
+                .map(id -> new Genre(id, "Genre_" + id))
+                .toList();
+    }
+
+}
