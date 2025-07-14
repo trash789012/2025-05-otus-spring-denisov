6a87c4b8585243f57ac42c081fca86a3adbc2da7
trash789012@gmail.com <trash789012@gmail.com>
2025-07-14 13:57:32 +0500

    убрал suppress

--- hw05/src/main/java/ru/otus/hw/commands/BookCommands.java
+++ hw05/src/main/java/ru/otus/hw/commands/BookCommands.java
@@ -9,7 +9,6 @@ import ru.otus.hw.services.BookService;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-@SuppressWarnings({"SpellCheckingInspection", "unused"})
 @RequiredArgsConstructor
 @ShellComponent
 public class BookCommands {
