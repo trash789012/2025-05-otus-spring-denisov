95c57a8a0f7696968c36877b71b2ba50d8db9cc1
trash789012@gmail.com <trash789012@gmail.com>
2025-07-15 11:20:10 +0500

    Dto author

--- hw06/src/main/java/ru/otus/hw/models/Author.java
+++ hw06/src/main/java/ru/otus/hw/models/Author.java
@@ -1,14 +1,20 @@
 package ru.otus.hw.models;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
-@Data
+@Setter
+@Getter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "authors")
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(name = "full_name", nullable = false)
     private String fullName;
 }
