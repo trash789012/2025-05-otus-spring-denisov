42b6ee7138b8ef2153b72557d236430028caa9a3
trash789012@gmail.com <trash789012@gmail.com>
2025-07-30 13:54:58 +0500

    Тесты для книг

--- hw08/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
+++ hw08/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.services;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
