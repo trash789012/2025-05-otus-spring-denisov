abbb62f0107a6b5531b8a21f1739d28bc2047fa8
trash789012@gmail.com <trash789012@gmail.com>
2025-07-30 13:54:43 +0500

    Тесты для книг

--- hw08/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
+++ hw08/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
@@ -1,162 +1,258 @@
 package ru.otus.hw.services;
-////
-////import org.junit.jupiter.api.DisplayName;
-////import org.junit.jupiter.api.Test;
-////import org.springframework.beans.factory.annotation.Autowired;
-////import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-////import org.springframework.context.annotation.Import;
-////import org.springframework.test.annotation.DirtiesContext;
-////import org.springframework.transaction.annotation.Propagation;
-////import org.springframework.transaction.annotation.Transactional;
-////import ru.otus.hw.converters.AuthorConverter;
-////import ru.otus.hw.converters.BookConverter;
-////import ru.otus.hw.converters.CommentConverter;
-////import ru.otus.hw.converters.GenreConverter;
-////import ru.otus.hw.dto.GenreDto;
-////import ru.otus.hw.exceptions.EntityNotFoundException;
-////
-////import java.util.Set;
-////
-////import static org.assertj.core.api.Assertions.assertThat;
-////import static org.assertj.core.api.Assertions.assertThatThrownBy;
-////
-//////@DisplayName("Интеграционный тест сервиса книг")
-//////@DataJpaTest
-//////@Import({BookServiceImpl.class,
-//////        BookConverter.class,
-//////        AuthorConverter.class,
-//////        GenreConverter.class,
-//////        CommentConverter.class})
-//////@Transactional(propagation = Propagation.NEVER)
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import ru.otus.hw.converters.AuthorConverter;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.converters.GenreConverter;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.GenreDto;
+import ru.otus.hw.models.Author;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.models.Genre;
+import ru.otus.hw.mongo.listener.BookCascadeDeleteMongoListener;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@DataMongoTest
+@DisplayName("Интеграционный тест сервиса книг ")
+@Import({BookServiceImpl.class,
+        BookConverter.class,
+        AuthorConverter.class,
+        GenreConverter.class,
+        BookCascadeDeleteMongoListener.class})
 public class BookServiceImplTest {
-//
-//    public static final long FIRST_BOOK_ID = 1L;
-//
-//    private final BookService bookService;
-//
-//    @Autowired
-//    public BookServiceImplTest(BookService bookService) {
-//        this.bookService = bookService;
-//    }
-//
-//    @Test
-//    @DisplayName("Должен находить книгу по id и всю информацию")
-//    public void findBookByIdShouldNotThrowLazyException() {
-//        var optionalBook = bookService.findById(FIRST_BOOK_ID);
-//        assertThat(optionalBook).isPresent();
-//
-//        var book = optionalBook.get();
-//
-//        //обращаемся ко всем элементам
-//        assertThat(book.id()).isGreaterThan(0).isEqualTo(FIRST_BOOK_ID);
-//        assertThat(book.title()).isNotBlank().isEqualTo("BookTitle_1");
-//
-//        assertThat(book.author()).isNotNull();
-//        assertThat(book.author().fullName()).isNotBlank().isEqualTo("Author_1");
-//
-//        assertThat(book.genres()).isNotEmpty().hasSize(2);
-//        assertThat(book.genres()).satisfies(genreDto -> {
-//            assertThat(genreDto.get(0).id()).isEqualTo(1L);
-//            assertThat(genreDto.get(0).name()).isNotBlank().isEqualTo("Genre_1");
-//            assertThat(genreDto.get(1).id()).isEqualTo(2L);
-//            assertThat(genreDto.get(1).name()).isNotBlank().isEqualTo("Genre_2");
-//        });
-//    }
-//
-//    @Test
-//    @DisplayName("Должен находить список всех книг и всю информацию")
-//    public void findAllBookShouldNotThrowLazyException() {
-//        var books = bookService.findAll();
-//
-//        assertThat(books).isNotNull();
-//        assertThat(books).allSatisfy(bookDto -> {
-//            //книга
-//            assertThat(bookDto.id()).isGreaterThan(0);
-//            assertThat(bookDto.title()).isNotBlank();
-//
-//            //автор
-//            assertThat(bookDto.author()).isNotNull();
-//            assertThat(bookDto.author().fullName()).isNotBlank();
-//
-//            //жанры
-//            assertThat(bookDto.genres()).isNotEmpty()
-//                    .allSatisfy(genreDto -> {
-//                        assertThat(genreDto.id()).isGreaterThan(0);
-//                        assertThat(genreDto.name()).isNotBlank();
-//                    });
-//        });
-//    }
-//
-//    @Test
-//    @DisplayName("Должен удалять книгу по id")
-//    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//    public void deleteBookByIdShouldNotThrowLazyException() {
-//        var optionalBook = bookService.findById(FIRST_BOOK_ID);
-//
-//        assertThat(optionalBook).isPresent();
-//
-//        bookService.deleteById(FIRST_BOOK_ID);
-//
-//        assertThat(bookService.findById(FIRST_BOOK_ID)).isEmpty();
-//    }
-//
-//    @Test
-//    @DisplayName("Должен создавать новую книгу")
-//    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//    public void insertBookShouldNotThrowLazyException() {
-//
-//        String title = "New Book Title";
-//        long authorId = 1L;
-//        Set<Long> genres = Set.of(1L, 2L);
-//
-//        var bookInserted = bookService.insert(title, authorId, genres);
-//
-//        assertThat(bookInserted).isNotNull();
-//        assertThat(bookInserted.id()).isGreaterThan(0);
-//        assertThat(bookInserted.title()).isEqualTo(title);
-//        assertThat(bookInserted.author()).isNotNull();
-//        assertThat(bookInserted.genres()).hasSize(2)
-//                .extracting(GenreDto::id)
-//                .containsExactlyInAnyOrderElementsOf(genres);
-//    }
-//
-//    @Test
-//    @DisplayName("Должен обновлять книгу")
-//    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-//    void updateBookShouldNotThrowLazyException() {
-//
-//        String title = "New Book Title";
-//        long authorId = 3L;
-//        Set<Long> genres = Set.of(5L, 6L);
-//
-//        var bookUpdated = bookService.update(FIRST_BOOK_ID, title, authorId, genres);
-//
-//        assertThat(bookUpdated.id()).isEqualTo(FIRST_BOOK_ID);
-//        assertThat(bookUpdated.title()).isEqualTo(title);
-//        assertThat(bookUpdated.author().id()).isEqualTo(authorId);
-//        assertThat(bookUpdated.genres()).hasSize(2)
-//                .extracting(GenreDto::id)
-//                .containsExactlyInAnyOrderElementsOf(genres);
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Должен бросать исключение при создании книги с несуществующим автором")
-//    void insertBookShoulThrowWithNoExistsAuthor() {
-//        assertThatThrownBy(() ->
-//                bookService.insert("Title", 99L, Set.of(1L, 2L)))
-//                .isInstanceOf(EntityNotFoundException.class)
-//                .hasMessageContaining("Author with id 99 not found");
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Должен бросать исключение при создании книги с несуществующим жанром")
-//    void insertBookShouldThrowWithNoExistsGenre() {
-//        assertThatThrownBy(() -> {
-//            bookService.insert("Title New", 1L, Set.of(99L));
-//        })
-//                .isInstanceOf(EntityNotFoundException.class)
-//                .hasMessageContaining("One or all genres with ids [99] not found");
-//    }
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private final BookService bookService;
+
+    private final AuthorConverter authorConverter;
+
+    private final GenreConverter genreConverter;
+
+    @Autowired
+    BookServiceImplTest(BookService bookService, AuthorConverter authorConverter,
+                        GenreConverter genreConverter) {
+        this.bookService = bookService;
+        this.authorConverter = authorConverter;
+        this.genreConverter = genreConverter;
+    }
+
+    @BeforeEach
+    void setUp() {
+        mongoTemplate.getDb().drop();
+    }
+
+    @Test
+    @DisplayName("должен находить книгу по ее id")
+    public void shouldFindBookById() {
+        Author author = new Author("Author");
+        mongoTemplate.save(author);
+
+        Genre genre1 = new Genre("Genre1");
+        Genre genre2 = new Genre("Genre2");
+
+        mongoTemplate.save(genre1);
+        mongoTemplate.save(genre2);
+
+        Book book = new Book("Book");
+        book.setAuthor(author);
+        book.setGenres(List.of(genre1, genre2));
+        mongoTemplate.save(book);
+
+        Optional<BookDto> savedBook = bookService.findById(book.getId());
+
+        assertThat(savedBook).isPresent();
+        assertAll(
+                () -> assertThat(savedBook.get())
+                        .extracting(BookDto::title, dto -> dto.author().fullName())
+                        .containsExactlyInAnyOrder("Book", "Author"),
+                () -> assertThat(savedBook.get().genres())
+                        .isNotNull()
+                        .hasSize(2)
+                        .extracting(GenreDto::name)
+                        .containsExactlyInAnyOrder("Genre1", "Genre2")
+        );
+    }
+
+    @Test
+    @DisplayName("должен возвращать все книги")
+    public void shouldFindAllBooks() {
+        Author author = new Author("Author");
+        mongoTemplate.save(author);
+
+        Genre genre1 = new Genre("Genre1");
+        Genre genre2 = new Genre("Genre2");
+
+        mongoTemplate.save(genre1);
+        mongoTemplate.save(genre2);
+
+        Book book1 = new Book("Book1");
+        book1.setAuthor(author);
+        book1.setGenres(List.of(genre1, genre2));
+        mongoTemplate.save(book1);
+
+        Book book2 = new Book("Book2");
+        book2.setAuthor(author);
+        book2.setGenres(List.of(genre1, genre2));
+        mongoTemplate.save(book2);
+
+        var allBooks = bookService.findAll();
+
+        assertAll(
+                () -> assertThat(allBooks).hasSize(2),
+                () -> assertThat(allBooks)
+                        .extracting(BookDto::title)
+                        .containsExactlyInAnyOrder("Book1", "Book2"),
+                () -> assertThat(allBooks)
+                        .extracting(dto -> dto.author().fullName())
+                        .containsOnly("Author"),
+                () -> assertThat(allBooks)
+                        .flatExtracting(BookDto::genres)
+                        .isNotNull()
+                        .hasSize(4)
+                        .extracting(GenreDto::name)
+                        .containsOnly("Genre1", "Genre2")
+
+        );
+    }
+
+    @Test
+    @DisplayName("должен создавать новую книгу")
+    void shouldCreateNewBook() {
+        Author author = new Author("Author");
+        mongoTemplate.save(author);
+
+        Genre genre1 = new Genre("Genre1");
+        Genre genre2 = new Genre("Genre2");
+
+        mongoTemplate.save(genre1);
+        mongoTemplate.save(genre2);
+
+        Book book = new Book("Book");
+        book.setAuthor(author);
+        book.setGenres(List.of(genre1, genre2));
+
+        BookDto bookDto = new BookDto(
+                null,
+                book.getTitle(),
+                authorConverter.authorToDto(book.getAuthor()),
+                book.getGenres().stream().map(genreConverter::genreToDto).toList()
+        );
+
+        var insertedBook = bookService.insert(bookDto);
+
+        var savedBook = mongoTemplate.findById(insertedBook.id(), Book.class);
+
+        assertAll(
+                () -> assertThat(insertedBook.title()).isEqualTo("Book"),
+                () -> assertThat(insertedBook.author().id()).isEqualTo(author.getId()),
+                () -> assertThat(insertedBook.genres())
+                        .isNotNull()
+                        .hasSize(2),
+                () -> assertThat(savedBook).isNotNull(),
+                () -> assertThat(savedBook.getAuthor().getId()).isEqualTo(author.getId()),
+                () -> assertThat(savedBook.getGenres())
+                        .extracting(Genre::getId)
+                        .containsExactlyInAnyOrder(genre1.getId(), genre2.getId())
+        );
+    }
+
+    @Test
+    @DisplayName("должен обновлять существующую книгу")
+    void shouldUpdateExistingBook() {
+        Author author = new Author("Author");
+        mongoTemplate.save(author);
+
+        Author author2 = new Author("New Author");
+        mongoTemplate.save(author2);
+
+        Genre genre1 = new Genre("Genre1");
+        mongoTemplate.save(genre1);
+
+        Genre genre2 = new Genre("New Genre");
+        mongoTemplate.save(genre2);
+
+        Book book = new Book("Book1");
+        book.setAuthor(author);
+        book.setGenres(List.of(genre1));
+        mongoTemplate.save(book);
+
+        BookDto bookDto = new BookDto(
+                book.getId(),
+                "New Title",
+                authorConverter.authorToDto(author2),
+                Stream.of(genre2).map(genreConverter::genreToDto).toList()
+        );
+
+        var updatedBook = bookService.update(bookDto);
+
+        var savedBook = mongoTemplate.findById(book.getId(), Book.class);
+
+        assertAll(
+                () -> assertThat(updatedBook.title()).isEqualTo("New Title"),
+                () -> assertThat(updatedBook.author().id()).isEqualTo(author2.getId()),
+                () -> assertThat(updatedBook.genres())
+                        .isNotNull()
+                        .hasSize(1)
+                        .extracting(GenreDto::name)
+                        .containsExactly("New Genre"),
+                () -> assertThat(savedBook).isNotNull().extracting(Book::getTitle)
+                        .isEqualTo("New Title"),
+                () -> {
+                    assert (savedBook != null ? savedBook.getAuthor() : null) != null;
+                    assertThat(savedBook.getAuthor().getId()).isEqualTo(author2.getId());
+                },
+                () -> {
+                    assert (savedBook != null ? savedBook.getGenres() : null) != null;
+                    assertThat(updatedBook.genres())
+                            .hasSize(1)
+                            .extracting(GenreDto::id)
+                            .containsExactly(genre2.getId());
+                }
+        );
+    }
+
+    @Test
+    @DisplayName("должен удалять книгу и каскадом комментарии")
+    void shouldDeleteExistingBook() {
+        Author author = new Author("Author");
+        mongoTemplate.save(author);
+
+        Book book = new Book("Book");
+        book.setAuthor(author);
+        book.setGenres(List.of());
+        mongoTemplate.save(book);
+
+        Comment comment1 = new Comment("Comment1");
+        comment1.setBook(book);
+        Comment comment2 = new Comment("Comment2");
+        comment2.setBook(book);
+        mongoTemplate.save(comment1);
+        mongoTemplate.save(comment2);
+
+        bookService.deleteById(book.getId());
+
+        var notExistsBook = mongoTemplate.findById(book.getId(), Book.class);
+        var notExistsComment1 = mongoTemplate.findById(comment1.getId(), Comment.class);
+        var notExistsComment2 = mongoTemplate.findById(comment2.getId(), Comment.class);
+
+        assertThat(notExistsBook).isNull();
+        assertThat(notExistsComment1).isNull();
+        assertThat(notExistsComment2).isNull();
+    }
 }
