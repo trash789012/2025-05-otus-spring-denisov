5d6ea452e1502e469b1478a4e620595eff2ce830
trash789012@gmail.com <trash789012@gmail.com>
2025-08-03 15:35:48 +0500

    checkstyle

--- hw09/src/main/java/ru/otus/hw/exceptions/GlobalExceptionHandler.java
+++ hw09/src/main/java/ru/otus/hw/exceptions/GlobalExceptionHandler.java
@@ -1,8 +1,6 @@
 package ru.otus.hw.exceptions;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
