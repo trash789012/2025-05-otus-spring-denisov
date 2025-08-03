9a1789de3ef472ebc67fb109eee72f53d6d718d8
trash789012@gmail.com <trash789012@gmail.com>
2025-08-03 16:59:35 +0500

    убрал EnableMongoRepo аннотацию

--- hw09/src/main/java/ru/otus/hw/mongo/config/MongoConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.hw.mongo.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-
-@Configuration
-@EnableMongoRepositories(basePackages = "ru.otus.hw.repositories")
-public class MongoConfig {
-}
