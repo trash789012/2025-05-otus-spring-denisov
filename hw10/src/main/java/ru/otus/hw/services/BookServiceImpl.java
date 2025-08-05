2624b71e149a6c5e080119946672a217dc9c4a40
trash789012@gmail.com <trash789012@gmail.com>
2025-08-05 12:54:30 +0500

    initial

---
+++ hw10/src/main/java/ru/otus/hw/services/BookServiceImpl.java
@@ -0,0 +1,94 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.converters.BookConverter;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.dto.BookFormDto;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.repositories.AuthorRepository;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.GenreRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.util.CollectionUtils.isEmpty;
+
+@RequiredArgsConstructor
+@Service
+public class BookServiceImpl implements BookService {
+    private final AuthorRepository authorRepository;
+
+    private final GenreRepository genreRepository;
+
+    private final BookRepository bookRepository;
+
+    private final BookConverter bookConverter;
+
+    @Override
+    public Optional<BookDto> findById(String id) {
+        return bookRepository.findById(id)
+                .map(bookConverter::bookToBookDto);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookConverter::bookToBookDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public BookDto insert(BookFormDto bookDto) {
+        return save(bookDto);
+    }
+
+    @Override
+    @Transactional
+    public BookDto update(BookFormDto bookDto) {
+        return save(bookDto);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(String id) {
+        bookRepository.deleteById(id);
+    }
+
+    private BookDto save(BookFormDto bookFormDto) {
+        if (isEmpty(bookFormDto.genreIds())) {
+            throw new IllegalArgumentException("Genres ids must not be null");
+        }
+
+        Book book;
+        book = prepareBook(bookFormDto, bookFormDto.genreIds());
+        return bookConverter.bookToBookDto(bookRepository.save(book));
+    }
+
+    private Book prepareBook(BookFormDto bookDto, List<String> genresIds) {
+        Book book;
+        if (bookDto.id() == null || bookDto.id().isEmpty()) {
+            book = new Book();
+        } else {
+            book = bookRepository.findById(bookDto.id()).orElseThrow(
+                    () -> new EntityNotFoundException("Book with id %s not found".formatted(bookDto.id()))
+            );
+        }
+        var author = authorRepository.findById(bookDto.authorId())
+                .orElseThrow(() -> new EntityNotFoundException(
+                        "Author with id %s not found".formatted(bookDto.authorId())));
+        var genres = genreRepository.findAllById(genresIds);
+        if (isEmpty(genres) || genresIds.size() != genres.size()) {
+            throw new EntityNotFoundException("One or all genres with ids %s not found".formatted(genresIds));
+        }
+        book.setId(bookDto.id());
+        book.setTitle(bookDto.title());
+        book.setAuthor(author);
+        book.setGenres(genres);
+        return book;
+    }
+}
---
+++ hw10/src/main/java/ru/otus/hw/services/CommentServiceImpl.java
@@ -0,0 +1,104 @@
+package ru.otus.hw.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.converters.CommentConverter;
+import ru.otus.hw.dto.CommentDto;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.Book;
+import ru.otus.hw.models.Comment;
+import ru.otus.hw.repositories.BookRepository;
+import ru.otus.hw.repositories.CommentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class CommentServiceImpl implements CommentService {
+
+    private final CommentRepository commentRepository;
+
+    private final CommentConverter commentConverter;
+
+    private final BookRepository bookRepository;
+
+    @Override
+    public Optional<CommentDto> findById(String id) {
+        return commentRepository.findById(id)
+                .map(commentConverter::commentToDto);
+    }
+
+    @Override
+    public List<CommentDto> findByBookId(String bookId) {
+
+        if (!bookRepository.existsById(bookId)) {
+            throw new EntityNotFoundException(
+                    "Book with id %s not found".formatted(bookId)
+            );
+        }
+
+        return commentRepository.findByBookId(bookId)
+                .stream().map(commentConverter::commentToDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public CommentDto insert(CommentDto commentDto) {
+        if (commentDto.bookId() == null) {
+            throw new IllegalArgumentException("Book id is empty");
+        }
+
+        return save(commentDto);
+    }
+
+    @Override
+    @Transactional
+    public CommentDto update(CommentDto commentDto) {
+        return save(commentDto);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(String id) {
+        commentRepository.deleteById(id);
+    }
+
+    private CommentDto save(CommentDto commentDto) {
+        Book book = null;
+        if (commentDto.bookId() != null) {
+            book = bookRepository.findById(commentDto.bookId()).orElseThrow(
+                    () -> new EntityNotFoundException("Book with id %d not found".formatted(commentDto.bookId()))
+            );
+        }
+
+        Comment comment = prepareComment(commentDto, book);
+
+        return commentConverter.commentToDto(commentRepository.save(comment));
+    }
+
+    private Comment prepareComment(CommentDto commentDto, Book book) {
+        Comment comment;
+        if (commentDto.id() == null) {
+            comment = new Comment();
+        } else {
+            comment = commentRepository.findById(commentDto.id())
+                    .orElseThrow(() ->
+                            new EntityNotFoundException(
+                                    "Comment with id %s not found".formatted(commentDto.id()))
+                    );
+        }
+
+        if (commentDto.text().isEmpty()) {
+            throw new IllegalArgumentException("Comment text is empty");
+        }
+
+        comment.setText(commentDto.text());
+        if (book != null) {
+            comment.setBook(book);
+        }
+        return comment;
+    }
+}
