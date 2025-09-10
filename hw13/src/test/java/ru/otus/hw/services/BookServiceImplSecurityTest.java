1f919ca0fd4cd424e41e0c58b8c7ac3b0dccd73c
trash789012@gmail.com <trash789012@gmail.com>
2025-09-10 13:33:06 +0500

    подготовка security тестов для сервисов

--- hw13/src/test/java/ru/otus/hw/services/BookServiceImplSecurityTest.java
+++ hw13/src/test/java/ru/otus/hw/services/BookServiceImplSecurityTest.java
@@ -1,9 +1,11 @@
 package ru.otus.hw.services;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.test.context.support.WithMockUser;
 import ru.otus.hw.config.acl.AclConfig;
@@ -15,8 +17,10 @@ import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.BookFormDto;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DataJpaTest()
 @Import({
@@ -36,7 +40,8 @@ public class BookServiceImplSecurityTest {
 
     @Test
     @WithMockUser(username = "user")
-    void shouldAllowOwnerToUpdateBook() {
+    @DisplayName("должен разрешать создателю обновлять книгу")
+    void shouldAllowUpdateBookWithSelfUser() {
         BookFormDto bookDto = new BookFormDto(0, "Title", 1, List.of(1L, 2L));
         BookDto createdBook = bookService.insert(bookDto);
 
@@ -44,5 +49,25 @@ public class BookServiceImplSecurityTest {
         BookDto updatedBook = bookService.update(bookUpdatedDto);
 
         assertThat(updatedBook.title()).isEqualTo("Upd Title");
+    }
+
+    @Test
+    @WithMockUser(username = "test")
+    @DisplayName("должен запрещать обновлять не свои книги")
+    void shouldDenyUpdateBookWithUngrantedUser() {
+        BookFormDto bookUpdatedDto = new BookFormDto(1L, "Upd Title", 1, List.of(1L, 2L));
+
+        assertThatThrownBy(() -> bookService.update(bookUpdatedDto))
+                .isInstanceOf(AccessDeniedException.class);
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    @DisplayName("должен разрешать удалять книгу тому, у кого есть полномочия")
+    void shouldAllowDeleteBookWithGrantedUser() {
+        bookService.deleteById(1L);
+
+        Optional<BookDto> deletedBook = bookService.findById(1L);
+        assertThat(deletedBook).isEmpty();
     }
 }
