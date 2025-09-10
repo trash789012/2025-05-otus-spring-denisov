d54a83088325519d669cdca08fc57a542074bda2
trash789012@gmail.com <trash789012@gmail.com>
2025-09-10 18:36:13 +0500

    комменты

--- hw12/src/main/java/ru/otus/hw/config/SecurityConfig.java
+++ hw12/src/main/java/ru/otus/hw/config/SecurityConfig.java
@@ -21,7 +21,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf(AbstractHttpConfigurer::disable)
                 .csrf(csrf -> csrf
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 )
