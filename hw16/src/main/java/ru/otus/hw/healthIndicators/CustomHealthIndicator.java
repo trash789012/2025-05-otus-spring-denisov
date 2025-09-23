85d380b3c4e63e46e99c7bee5b577ab63c322d0e
trash789012@gmail.com <trash789012@gmail.com>
2025-09-23 11:14:24 +0500

    health check + HATEOAS

---
+++ hw16/src/main/java/ru/otus/hw/healthIndicators/CustomHealthIndicator.java
@@ -0,0 +1,39 @@
+package ru.otus.hw.healthIndicators;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+import ru.otus.hw.exceptions.EntityNotFoundException;
+import ru.otus.hw.models.User;
+import ru.otus.hw.repositories.UserRepository;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class CustomHealthIndicator implements HealthIndicator {
+
+    public static final String ROOT_USER = "admin";
+    private final UserRepository userRepository;
+
+    @Override
+    public Health health() {
+
+        try {
+            Optional<User> user = userRepository.findByUsername(ROOT_USER);
+            if (user.isEmpty()) {
+                throw new EntityNotFoundException("User not found");
+            }
+            return Health.up()
+                    .withDetail("username", user.get().getUsername())
+                    .withDetail("status", "exists")
+                    .build();
+        } catch (Exception ex) {
+            return Health.down()
+                    .withDetail("username", ROOT_USER)
+                    .withDetail("status", "not exists")
+                    .build();
+        }
+    }
+}
--- hw16/src/main/java/ru/otus/hw/repositories/CommentRepository.java
+++ hw16/src/main/java/ru/otus/hw/repositories/CommentRepository.java
@@ -1,12 +1,16 @@
 package ru.otus.hw.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import ru.otus.hw.models.Comment;
 
 import javax.annotation.Nonnull;
 import java.util.List;
 
+@RepositoryRestResource(path = "comment")
 public interface CommentRepository extends CrudRepository<Comment, Long> {
+    @RestResource(path = "bookId", rel = "bookId")
     @Nonnull
     List<Comment> findByBookId(long bookId);
 }
