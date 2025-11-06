3d5f086c9bc473f0d6b162b4870308ff05825f83
trash789012@gmail.com <trash789012@gmail.com>
2025-11-06 11:59:59 +0500

    проверка REST Controller для слотов

--- project/src/main/java/ru/otus/hw/controllers/rest/SlotRestController.java
+++ project/src/main/java/ru/otus/hw/controllers/rest/SlotRestController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.otus.hw.dto.SlotDto;
@@ -18,27 +19,28 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/slot")
 public class SlotRestController {
 
     private final SlotService slotService;
 
-    @GetMapping("/api/v1/slot")
+    @GetMapping
     public List<SlotDto> getAllSlots() {
         return slotService.findAll();
     }
 
-    @GetMapping("/api/v1/slot/{id}")
+    @GetMapping("/{id}")
     public SlotDto getSlotById(@PathVariable Long id) {
         return slotService.findById(id);
     }
 
-    @PostMapping("/api/v1/slot")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public SlotDto createSlot(@RequestBody SlotDto slotDto) {
         return slotService.insert(slotDto);
     }
 
-    @PutMapping("/api/v1/slot/{id}")
+    @PutMapping("/{id}")
     public SlotDto updateSlot(@PathVariable Long id,
                               @RequestBody SlotDto slotDto) {
         if (!id.equals(slotDto.id())) {
@@ -48,7 +50,7 @@ public class SlotRestController {
         return slotService.update(slotDto);
     }
 
-    @DeleteMapping("/api/v1/slot/{id}")
+    @DeleteMapping("/{id}")
     public void deleteSlot(@PathVariable Long id) {
         slotService.delete(id);
     }
