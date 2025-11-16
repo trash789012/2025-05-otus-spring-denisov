4bd9a0dc73cb033006d68218b0c9265f0cefcd8e
trash789012@gmail.com <trash789012@gmail.com>
2025-11-16 13:24:51 +0500

    подключаю докер

--- project/src/main/java/ru/otus/hw/JamTimeApplication.java
+++ project/src/main/java/ru/otus/hw/JamTimeApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class JamTimeApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(JamTimeApplication.class, args);
         System.out.printf("Чтобы перейти на страницу сайта: %n%s%n",
                 "http://localhost:8080");
     }
--- project/src/main/java/ru/otus/hw/config/security/SecurityConfig.java
+++ project/src/main/java/ru/otus/hw/config/security/SecurityConfig.java
@@ -46,6 +46,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/login/**",
                                 "/",
+                                "/favicon.ico",
                                 "/profile/**",
                                 "/admin/**",
                                 "/css/**",
